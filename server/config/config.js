55369974b003f7875b51a8b619e20b6d5ba5cc97
unknown <ma2009ji10@yahoo.com>
2018-11-16 00:48:03 -0600

    Variable MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev'){
      urlDB = 'mongodb://localhost:27017/cafe'
 }else{
-     urlDB = 'mongodb://cafe-user:123456X@ds263493.mlab.com:63493/cafe'
+     urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDB = urlDB;
