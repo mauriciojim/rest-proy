e32c9d1f1738257074255d7f94e60aaf3449b6ab
unknown <ma2009ji10@yahoo.com>
2018-11-16 00:37:59 -0600

    Segudno comit

--- server/config/config.js
+++ server/config/config.js
@@ -8,10 +8,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
 let urlDB;
 
-// if( process.env.NODE_ENV === 'dev'){
-//     urlDB = 'mongodb://localhost:27017/cafe'
-// }else{
+if( process.env.NODE_ENV === 'dev'){
+     urlDB = 'mongodb://localhost:27017/cafe'
+}else{
      urlDB = 'mongodb://cafe-user:123456X@ds263493.mlab.com:63493/cafe'
-// }
+}
 
 process.env.urlDB = urlDB;
